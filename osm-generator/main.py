c28e22b4a9d7b5b9e3cbf75749adca96516e6853
jpasierbb <kubapasierb1@wp.pl>
2024-09-28 21:18:13 +0000

    Created main module with test loc_names

---
+++ osm-generator/main.py
@@ -0,0 +1,7 @@
+from omnx import OSMGenerator
+
+
+# START_LOC_NAME = 'Skała, Poland'
+# END_LOC_NAME = 'Krzeszowice, Poland'
+START_LOC_NAME = 'Centralna 38, Wielka wieś, Poland'
+END_LOC_NAME = 'Szyce 101, Poland'
